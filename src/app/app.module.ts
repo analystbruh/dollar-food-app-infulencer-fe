4e4d62299a53e493919ae872bfda5e696ab41620
analystbruh <chris.martin.data@gmail.com>
2022-12-19 09:51:59 -0500

    more wip

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,24 +2,30 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { VidGridComponent } from './home/vid-grid/vid-grid.component';
+import { SideNavComponent } from './side-nav/side-nav.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    VidGridComponent
+    VidGridComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatCardModule,
-    MatButtonModule
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
