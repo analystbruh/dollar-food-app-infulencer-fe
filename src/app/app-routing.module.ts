df75ccbb1861acb6d0c6c450316e9860ef4d8121
analystbruh <chris.martin.data@gmail.com>
2023-01-03 10:05:35 -0500

    added near me page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { VidGridLocComponent } from './vid-grid-loc/vid-grid-loc.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent }
+  { path: '', component: HomeComponent },
+  { path: 'near-me', component: VidGridLocComponent}
 ];
 
 @NgModule({
