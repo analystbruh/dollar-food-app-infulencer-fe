2d001f2fbb203f05873e6b7f2f8a0205db27e53e
analystbruh <chris.martin.data@gmail.com>
2023-01-22 11:57:09 -0500

    updated links, added pics

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -32,12 +32,12 @@ export class HomeComponent implements OnInit {
     {
       videoLink: 'https://www.youtube.com/embed/iJqayQ1mEqg',
       dish: 'Pork with Garlic',
-      price: 22,
-      deliveryLink:'https://buy.stripe.com/eVacOabbG2hQeoEfZ1',
+      price: 15,
+      deliveryLink:'https://buy.stripe.com/cN29BY93ye0ydkA3cg',
       pickupLink: 'https://buy.stripe.com/14kdSebbG5u21BS4gg',
       tipLink: 'https://buy.stripe.com/9AQ7tQbbGg8G80g4gi',
-      influencerPic: 'assets/IMG_1659.jpg',
-      foodPic: 'assets/IMG_3282.jpg'
+      influencerPic: 'assets/christiana.png',
+      foodPic: 'assets/orangechicken.png'
     },
   ].map(obj => {return {
     videoLink: this.sanitize(obj.videoLink),
