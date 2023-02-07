00718ed6b8ff37c3bb1cb801b53b70dd15ab6b0f
analystbruh <chris.martin.data@gmail.com>
2023-02-07 10:09:04 -0500

    added new footer component

---
+++ src/app/footer2/footer2.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { Footer2Component } from './footer2.component';
+
+describe('Footer2Component', () => {
+  let component: Footer2Component;
+  let fixture: ComponentFixture<Footer2Component>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ Footer2Component ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(Footer2Component);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
