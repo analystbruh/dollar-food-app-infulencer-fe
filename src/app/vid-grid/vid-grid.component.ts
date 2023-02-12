dc6b70d974ac8e01dfef84963a4a66afa97fca01
analystbruh <chris.martin.data@gmail.com>
2023-02-12 11:32:11 -0500

    enable test for autoplay, cleaned up unnecessary code

--- src/app/vid-grid/vid-grid.component.ts
+++ src/app/vid-grid/vid-grid.component.ts
@@ -3,6 +3,7 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 interface CardData {
   videoLink: SafeResourceUrl;
+  showMe: boolean;
   thumb: string;
   dish: string;
   price: number;
@@ -21,43 +22,12 @@ interface CardData {
   styleUrls: ['./vid-grid.component.css']
 })
 export class VidGridComponent implements OnInit {
+
   @Input() shorts!: CardData[];
-
-  // public shorts: CardData[] = [
-    // {
-    //   videoLink: 'https://www.youtube.com/embed/2FJORVxKdwE',
-    //   dish: 'Crazy Skewers',
-    //   price: 21,
-    //   deliveryLink: 'https://buy.stripe.com/test_aEU7tif2g7oK2OI9AD',
-    //   pickupLink: 'https://buy.stripe.com/test_28o00Q3jy5gCgFy5ko',
-    //   tipLink: 'https://buy.stripe.com/test_4gw00Q07mbF0ahaeUW',
-    //   influencerPic: 'assets/IMG_1659.jpg',
-    //   foodPic: 'assets/skewers.png'
-    // },
-    // {
-    //   videoLink: 'https://www.youtube.com/embed/iJqayQ1mEqg',
-    //   dish: 'Pork with Garlic',
-    //   price: 2,
-    //   deliveryLink:'https://buy.stripe.com/aEU15s93y7Ca6Wc9AB',
-    //   pickupLink: 'https://buy.stripe.com/14kdSebbG5u21BS4gg',
-    //   tipLink: 'https://buy.stripe.com/9AQ7tQbbGg8G80g4gi',
-    //   influencerPic: 'assets/IMG_1659.jpg',
-    //   foodPic: 'assets/IMG_3282.jpg'
-    // }
-  // ].map(obj => {return {
-  //   videoLink: this.sanitize(obj.videoLink),
-  //   dish: obj.dish,
-  //   price: obj.price,
-  //   deliveryLink: this.sanitize(obj.deliveryLink),
-  //   pickupLink: this.sanitize(obj.pickupLink),
-  //   tipLink: this.sanitize(obj.tipLink),
-  //   influencerPic: obj.influencerPic,
-  //   foodPic: obj.foodPic
-  // }})
-
 
   constructor(public s: DomSanitizer) { }
 
+  public videoNum!: number;
 
 
   ngOnInit(): void {
@@ -68,12 +38,10 @@ export class VidGridComponent implements OnInit {
     return this.s.bypassSecurityTrustResourceUrl(url)
   }
 
-  playVid(vidID: string, thumbID: string): void {
+  playVid(thumbID: string, short: any): void {
     let thumb = document.getElementById(thumbID) as HTMLDivElement;
-    let video = document.getElementById(vidID) as HTMLDivElement;
     thumb.style.display = 'none';
-    video.style.display = 'block';
-    console.log(video)
+    short.showMe = true;
   }
 
 }
