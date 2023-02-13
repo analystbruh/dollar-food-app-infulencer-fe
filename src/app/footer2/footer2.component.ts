e86566175a673bbee801c08b0987314eede28147
Alex Lu <71916027+Sugaminni@users.noreply.github.com>
2023-02-13 04:33:46 -0500

    Added Animation Components

--- src/app/footer2/footer2.component.ts
+++ src/app/footer2/footer2.component.ts
@@ -1,9 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-footer2',
   templateUrl: './footer2.component.html',
-  styleUrls: ['./footer2.component.css']
+  styleUrls: ['./footer2.component.css'],
+  animations: [
+    trigger('fadeIn', [
+      state('void', style({ opacity: 0, transform: 'translateY(100%)' })),
+      transition('void => *', animate(1000)),
+    ]),
+  ],
 })
 export class Footer2Component implements OnInit {
 
@@ -13,3 +20,4 @@ export class Footer2Component implements OnInit {
   }
 
 }
+
